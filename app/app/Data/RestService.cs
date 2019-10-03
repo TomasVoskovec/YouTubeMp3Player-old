089c0a3dee72e00ffb31fcba92697277038419e3
Tomas Voskovec <tomas.voskovec@gmail.com>
2019-10-03 10:47:27 +0200

    init

--- app/app/Data/RestService.cs
+++ app/app/Data/RestService.cs
@@ -17,7 +17,7 @@ namespace app.Data
         {
             client = new HttpClient();
             client.MaxResponseContentBufferSize = 256000;
-            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application(x-www-form-urlencoded' "));
+            client.DefaultRequestHeaders.Accept.Add(new System.Net.Http.Headers.MediaTypeWithQualityHeaderValue("application/x-www-form-urlencoded"));
         }
 
         public async Task<Token> Login(User user)
